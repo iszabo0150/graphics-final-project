a7aa1a680b4ddbc5775d81869619954d32ddb73c
ethanmcdowell <ethan_mcdowell@brown.edu>
2025-12-10 18:45:23 -0500

    Fix cubemap face order

---
+++ build/build-final_proj-Qt_6_9_2_macos_f0f59d-Debug/CMakeFiles/final_proj.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "final_proj_autogen/timestamp" "custom" "final_proj_autogen/deps"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/build/build-final_proj-Qt_6_9_2_macos_f0f59d-Debug/.qt/rcc/qrc_Resources.cpp" "CMakeFiles/final_proj.dir/build/build-final_proj-Qt_6_9_2_macos_f0f59d-Debug/.qt/rcc/qrc_Resources.cpp.o" "gcc" "CMakeFiles/final_proj.dir/build/build-final_proj-Qt_6_9_2_macos_f0f59d-Debug/.qt/rcc/qrc_Resources.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/build/build-final_proj-Qt_6_9_2_macos_f0f59d-Debug/final_proj_autogen/mocs_compilation.cpp" "CMakeFiles/final_proj.dir/final_proj_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/final_proj.dir/final_proj_autogen/mocs_compilation.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/camera/camera.cpp" "CMakeFiles/final_proj.dir/src/camera/camera.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/camera/camera.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/lsystem/lsystem.cpp" "CMakeFiles/final_proj.dir/src/lsystem/lsystem.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/lsystem/lsystem.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/lsystem/plantpresets.cpp" "CMakeFiles/final_proj.dir/src/lsystem/plantpresets.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/lsystem/plantpresets.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/lsystem/presets/bush.cpp" "CMakeFiles/final_proj.dir/src/lsystem/presets/bush.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/lsystem/presets/bush.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/lsystem/presets/flower_plant.cpp" "CMakeFiles/final_proj.dir/src/lsystem/presets/flower_plant.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/lsystem/presets/flower_plant.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/lsystem/presets/oak_tree.cpp" "CMakeFiles/final_proj.dir/src/lsystem/presets/oak_tree.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/lsystem/presets/oak_tree.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/main.cpp" "CMakeFiles/final_proj.dir/src/main.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/main.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/mainwindow.cpp" "CMakeFiles/final_proj.dir/src/mainwindow.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/mainwindow.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/particlesystem.cpp" "CMakeFiles/final_proj.dir/src/particlesystem.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/particlesystem.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/postprocess.cpp" "CMakeFiles/final_proj.dir/src/postprocess.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/postprocess.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/realtime/realtime.cpp" "CMakeFiles/final_proj.dir/src/realtime/realtime.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/realtime/realtime.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/renderers/godrayrenderer.cpp" "CMakeFiles/final_proj.dir/src/renderers/godrayrenderer.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/renderers/godrayrenderer.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/renderers/lightrenderer.cpp" "CMakeFiles/final_proj.dir/src/renderers/lightrenderer.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/renderers/lightrenderer.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/renderers/scenerenderer.cpp" "CMakeFiles/final_proj.dir/src/renderers/scenerenderer.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/renderers/scenerenderer.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/renderers/screenrenderer.cpp" "CMakeFiles/final_proj.dir/src/renderers/screenrenderer.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/renderers/screenrenderer.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/renderers/shaperenderer.cpp" "CMakeFiles/final_proj.dir/src/renderers/shaperenderer.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/renderers/shaperenderer.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/settings.cpp" "CMakeFiles/final_proj.dir/src/settings.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/settings.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/shapes/conetesselator.cpp" "CMakeFiles/final_proj.dir/src/shapes/conetesselator.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/shapes/conetesselator.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/shapes/cubetesselator.cpp" "CMakeFiles/final_proj.dir/src/shapes/cubetesselator.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/shapes/cubetesselator.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/shapes/cylindertesselator.cpp" "CMakeFiles/final_proj.dir/src/shapes/cylindertesselator.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/shapes/cylindertesselator.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/shapes/meshloader.cpp" "CMakeFiles/final_proj.dir/src/shapes/meshloader.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/shapes/meshloader.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/shapes/spheretesselator.cpp" "CMakeFiles/final_proj.dir/src/shapes/spheretesselator.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/shapes/spheretesselator.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/utils/objloader.cpp" "CMakeFiles/final_proj.dir/src/utils/objloader.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/utils/objloader.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/utils/scenefilereader.cpp" "CMakeFiles/final_proj.dir/src/utils/scenefilereader.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/utils/scenefilereader.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/utils/sceneparser.cpp" "CMakeFiles/final_proj.dir/src/utils/sceneparser.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/utils/sceneparser.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/utils/terraingenerator.cpp" "CMakeFiles/final_proj.dir/src/utils/terraingenerator.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/utils/terraingenerator.cpp.o.d"
+  "/Users/macdeal/Desktop/CSCI1230/graphics-final-project/src/utils/textureutils.cpp" "CMakeFiles/final_proj.dir/src/utils/textureutils.cpp.o" "gcc" "CMakeFiles/final_proj.dir/src/utils/textureutils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/build-final_proj-Qt_6_9_2_macos_f0f59d-Release/CMakeFiles/StaticGLEW.dir/StaticGLEW_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/StaticGLEW.dir/StaticGLEW_autogen/mocs_compilation.cpp.o: \
+  /Users/macdeal/Desktop/CSCI1230/graphics-final-project/build/build-final_proj-Qt_6_9_2_macos_f0f59d-Release/StaticGLEW_autogen/mocs_compilation.cpp
